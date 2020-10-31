567ab27c50f0d592b18cdc17b759f5431eb5d820
jiayi-ren <jiayiren95@gmail.com>
2020-10-30 23:17:11 -0700

    db config, knex migration

---
+++ data/migrations/20201030092335_create-users-table.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex) {
+  return knex.schema
+    // .raw('CREATE EXTENSIOn IF NOT EXISTS "uuid-oosp"')
+    .createTable("users", function (table) {
+        table.string("id").notNullable().unique().primary();
+        table.string("name");
+        table.string("email");
+        table.string("picture");
+        table.timestamps(true, true);
+    })
+};
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTableIfExists("users");
+};

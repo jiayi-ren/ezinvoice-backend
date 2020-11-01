05057a39a7f68720d9a2027f843a931064467756
jiayi-ren <jiayiren95@gmail.com>
2020-11-01 13:10:22 -0800

    modify users table

--- data/migrations/20201030092335_create-users-table.js
+++ data/migrations/20201030092335_create-users-table.js
@@ -4,10 +4,10 @@ exports.up = async function(knex) {
     .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
     .createTable("users", function (table) {
         table.increments();
-        table.uuid("uuid").defaultTo(knex.raw("uuid_generate_v4()"));
         table.string("name");
         table.string("email");
         table.string("picture");
+        table.string("sub");
         table.timestamps(true, true);
     })
 };

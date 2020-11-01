ac4f4a2392df69766a302287247fb0ecd6393a4d
jiayi-ren <jiayiren95@gmail.com>
2020-10-31 22:50:50 -0700

    modify users table

--- data/migrations/20201030092335_create-users-table.js
+++ data/migrations/20201030092335_create-users-table.js
@@ -1,9 +1,10 @@
 
-exports.up = function(knex) {
-  return knex.schema
-    .raw('CREATE EXTENSION IF NOT EXISTS "uuid-oosp"')
+exports.up = async function(knex) {
+  return await knex.schema
+    .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
     .createTable("users", function (table) {
-        table.string("id").notNullable().unique().primary();
+        table.increments();
+        table.uuid("uuid").defaultTo(knex.raw("uuid_generate_v4()"));
         table.string("name");
         table.string("email");
         table.string("picture");

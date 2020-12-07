0b687c6a13d5b281dc4bb168748a0f9129f19bda
Jiayi Ren <45396888+jiayi-ren@users.noreply.github.com>
2020-12-07 15:25:17 -0800

    add business model

---
+++ api/business/businessModel.js
@@ -0,0 +1,57 @@
+const db = require('../../data/db-config');
+
+const findAll = async () => {
+    return await db('businesses').select('*');
+};
+
+const findById = async id => {
+    return await db('businesses').where({ id }).first().select('*');
+};
+
+const findByEmail = async email => {
+    return await db('businesses').where({ email: email }).first().select('*');
+};
+
+const findAllByUserId = async user_id => {
+    return await db('businesses').where({ user_id: user_id }).select('*');
+};
+
+const create = async business => {
+    return await db('businesses').insert(business).returning('*');
+};
+
+const update = async (id, business) => {
+    return await db('businesses')
+        .where({ id })
+        .first()
+        .update(business)
+        .returning('*');
+};
+
+const remove = async id => {
+    return await db('businesses').where({ id }).del();
+};
+
+const findOrCreateBusiness = async business => {
+    const foundBusiness = await findByEmail(business.email).then(
+        business => business,
+    );
+    if (foundBusiness) {
+        return foundBusiness;
+    } else {
+        return await create(business).then(newBusiness => {
+            return newBusiness ? newBusiness[0] : newBusiness;
+        });
+    }
+};
+
+module.exports = {
+    findAll,
+    findById,
+    findByEmail,
+    findAllByUserId,
+    create,
+    update,
+    remove,
+    findOrCreateBusiness,
+};

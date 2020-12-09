9fc9138e867a2406fa2796d335eef731f28e682f
Jiayi Ren <45396888+jiayi-ren@users.noreply.github.com>
2020-12-08 17:20:13 -0800

    fix migrations order

---
+++ data/migrations/20201208171612_create-usersettings-table.js
@@ -0,0 +1,28 @@
+const { onUpdateTrigger } = require('../../config/knexfile');
+
+exports.up = async function (knex) {
+    return await knex.schema
+        .createTable('user_settings', function (table) {
+            table.bigIncrements();
+            table.string('name');
+            table.string('email');
+            table.string('street');
+            table.string('city_state');
+            table.string('zip');
+            table.string('phone');
+            table
+                .bigInteger('user_id')
+                .unsigned()
+                .notNullable()
+                .references('id')
+                .inTable('users')
+                .onUpdate('CASCADE')
+                .onDelete('CASCADE');
+            table.timestamps(true, true);
+        })
+        .then(() => knex.raw(onUpdateTrigger('user_settings')));
+};
+
+exports.down = function (knex) {
+    return knex.schema.dropTableIfExists('user_settings');
+};

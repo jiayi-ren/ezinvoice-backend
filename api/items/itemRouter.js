afa5d0ccd9afcb34376eb09a9c4a3ee81a4007e4
Jiayi Ren <45396888+jiayi-ren@users.noreply.github.com>
2021-03-13 19:41:25 -0800

    integrate algolia and transaction to items router

--- api/items/itemRouter.js
+++ api/items/itemRouter.js
@@ -3,6 +3,13 @@ const createError = require('http-errors');
 const authRequired = require('../middleware/authRequired');
 const router = express.Router();
 const Items = require('./itemModel');
+const algoliasearch = require('algoliasearch');
+const client = algoliasearch(
+    process.env.INSTANT_SEARCH_APPLICATION_ID,
+    process.env.INSTANT_SEARCH_ADMIN_API_KEY,
+);
+const index = client.initIndex('items');
+const knex = require('../../data/db-config');
 
 /**
  * @swagger
@@ -85,43 +92,71 @@ const Items = require('./itemModel');
  *        $ref: '#/components/responses/InternalServerError'
  */
 
-router.post('/', authRequired, (req, res, next) => {
+router.post('/', authRequired, async (req, res, next) => {
     let itemReq = req.body;
     const authUserId = req.user.id;
     itemReq.user_id = authUserId;
 
-    Items.findByDescription(itemReq.description)
-        .then(item => {
-            if (item) {
-                next(
-                    createError(
-                        409,
-                        `Item with description ${itemReq.description} already exists`,
-                        { expose: true },
-                    ),
-                );
-            }
-            Items.create(itemReq)
-                .then(item => {
-                    if (item) {
-                        return res.status(201).json({
-                            message: 'Successfully create the item',
-                            item: item[0],
-                        });
-                    }
+    const trx = await knex.transaction();
+    try {
+        Items.findByDescription(itemReq.description)
+            .then(item => {
+                if (item) {
                     next(
                         createError(
-                            500,
-                            'Failed to create a item for the user',
-                            {
-                                expose: true,
-                            },
+                            409,
+                            `Item with description ${itemReq.description} already exists`,
+                            { expose: true },
                         ),
                     );
-                })
-                .catch(err => next(err));
-        })
-        .catch(err => next(err));
+                }
+                index
+                    .saveObjects([itemReq], {
+                        autoGenerateObjectIDIfNotExist: true,
+                    })
+                    .then(async ({ objectIDs }) => {
+                        itemReq.id = objectIDs[0];
+                        await Items.create(itemReq)
+                            .then(async item => {
+                                if (item) {
+                                    return await Items.showItem(item[0].id)
+                                        .then(async item => {
+                                            if (item) {
+                                                trx.commit();
+                                                return await res
+                                                    .status(201)
+                                                    .json({
+                                                        message:
+                                                            'Successfully create the item',
+                                                        item: item[0],
+                                                    });
+                                            }
+                                            throw next(500);
+                                        })
+                                        .catch(err => {
+                                            index.deleteObject(itemReq.id);
+                                            throw next(err);
+                                        });
+                                }
+                                index.deleteObject(itemReq.id);
+                                throw next(500);
+                            })
+                            .catch(err => {
+                                index.deleteObject(itemReq.id);
+                                throw next(err);
+                            });
+                    })
+                    .catch(err => {
+                        throw next(err);
+                    });
+            })
+            .catch(err => {
+                throw next(err);
+            });
+    } catch (err) {
+        trx.rollback();
+        throw next(err);
+    }
 });
 
 /**
@@ -201,44 +236,69 @@ router.get('/', authRequired, (req, res) => {
  *        description: 'Internal Server Error'
  */
 
-router.put('/:id', authRequired, (req, res, next) => {
+router.put('/:id', authRequired, async (req, res, next) => {
     const id = req.params.id;
     const itemReq = req.body;
     const authUserId = req.user.id;
 
-    if (authUserId === itemReq.user_id) {
-        return Items.findById(id)
-            .then(item => {
-                if (item) {
-                    if (item.id === id && item.id === itemReq.id) {
-                        return Items.update(id, itemReq)
-                            .then(updated => {
-                                res.status(200).json({
-                                    message: `Successfully updated item ${item.id}`,
-                                    item: updated[0],
+    const trx = await knex.transaction();
+    try {
+        if (authUserId === itemReq.user_id) {
+            return Items.findById(id)
+                .then(async item => {
+                    if (item) {
+                        if (item.id === id && item.id === itemReq.id) {
+                            return Items.update(id, itemReq)
+                                .then(async ([updatedItem]) => {
+                                    await index
+                                        .partialUpdateObject({
+                                            objectID: updatedItem.id,
+                                            description:
+                                                updatedItem.description,
+                                            rate: updatedItem.rate,
+                                        })
+                                        .then(() => {
+                                            trx.commit();
+                                            return res.status(200).json({
+                                                message: `Successfully updatedItem item ${updatedItem.id}`,
+                                                item: updatedItem,
+                                            });
+                                        })
+                                        .catch(err => {
+                                            throw next(err);
+                                        });
+                                })
+                                .catch(err => {
+                                    throw next(err);
                                 });
-                            })
-                            .catch(err => next(err));
+                        }
+                        throw next(
+                            createError(
+                                400,
+                                'Item id doest not match with record',
+                                {
+                                    expose: true,
+                                },
+                            ),
+                        );
                     }
-                    return next(
-                        createError(
-                            400,
-                            'Item id doest not match with record',
-                            {
-                                expose: true,
-                            },
-                        ),
+                    throw next(
+                        createError(404, 'Item not found for current user', {
+                            expose: true,
+                        }),
                     );
-                }
-                next(
-                    createError(404, 'Item not found for current user', {
-                        expose: true,
-                    }),
-                );
-            })
-            .catch(err => next(err));
+                })
+                .catch(err => {
+                    throw next(err);
+                });
+        }
+        throw next(
+            (401, 'Not authorized to complete this request', { expose: true }),
+        );
+    } catch (err) {
+        trx.rollback();
+        throw next(err);
     }
-    next((401, 'Not authorized to complete this request', { expose: true }));
 });
 
 /**
@@ -268,14 +328,20 @@ router.delete('/:id', authRequired, (req, res, next) => {
     const authUserId = req.user.id;
 
     Items.findById(id)
-        .then(item => {
+        .then(async item => {
             if (item) {
                 if (item.user_id === authUserId) {
-                    return Items.remove(item.id)
-                        .then(() => {
-                            res.status(200).json({
-                                message: 'Successfully deleted the item',
-                            });
+                    return await Items.remove(item.id)
+                        .then(async () => {
+                            await index
+                                .deleteObject(item.id)
+                                .then(() => {
+                                    res.status(200).json({
+                                        message:
+                                            'Successfully deleted the item',
+                                    });
+                                })
+                                .catch(err => next(err));
                         })
                         .catch(err => next(err));
                 }
